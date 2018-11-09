0c69f7a245781a8cf268fc5b74f0516deeac97ac
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-11-08 20:32:07 -0500

    update master

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  // initialize state
+  state = { msg: '' }
+
+  // fetch msg after mounting
+  componentDidMount(){
+    this.getMsg();
+  }
+
+  getMsg = () => {
+    fetch('/api/msg')
+      .then(res => res.json())
+      .then(msg => this.setState( {msg}))
+  }
+
+  render() {
+    const { msg } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            { msg }
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
